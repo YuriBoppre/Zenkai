c9c7d9e48c4468cdded72cd1a5010967387d4f2e
YuriBoppre <yuriboppre@gmail.com>
2023-06-26 14:56:17 -0300

    Detalhes no layout

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Sidebar_Itens } from "./Sidebar_Itens";
 import "./Sidebar.css";
 import { Link } from "react-router-dom";
+import Zenkai from "../../Assets/zenkaiLogo.png";
 
 const Sidebar = () => {
   return (
@@ -9,7 +10,8 @@ const Sidebar = () => {
       <div className="sidebar">
         <div className="sidebarWrapper">
           <div className="sidebarMenu">
-            <h3 className="title">Zenkai</h3>
+            <h3 className="title">Atalhos</h3>
+            {/* <img src={Zenkai} alt="logo" /> */}
             <ul className="sidebarList">
               <Link to="/page/" className="link">
                 <li className="sidebarListItem">Tickets</li>
